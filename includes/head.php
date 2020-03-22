1158b6b41d4695d4f836053cf4982d421137a281
Nick Okonek <nickokonek@gmail.com>
2020-03-22 11:48:25 -0500

    added jquery and mobile menu scripts and styles

--- includes/head.php
+++ includes/head.php
@@ -9,5 +9,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet/less" type="text/css" href="less/styles.less" />
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
+  <script src="scripts/mm.js"></script>
 </head>
