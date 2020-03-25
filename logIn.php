55fd6099100b82ba70255c4dd8921bebdcbdac59
Mike K <callmeknuts@gmail.com>
2020-03-25 14:45:24 -0500

    Extremly rough draft of logIn.php/register.php

--- logIn.php
+++ logIn.php
@@ -1,18 +1,19 @@
-<!DOCTYPE html>
-<html>
 <?php 
   require 'includes/head.php';
   require 'includes/body.php';
   require 'includes/header.php';
-?>
-<!--  Do we need a "Log-in to your account" banner here, similiar to design mock-ups  -->
 
+  $PageTitle = "Your Expeditions Start Here"
+?>
+    
+  <div class="page-title">
+    <h2><?php echo $PageTitle ?></h2>
+  </div>
 
   <div class="logInBanner">
     <h2>LOGIN TO YOUR ACCOUNT</h2>
   </div>
 
-<body>
 <!-- div container for all content in log-in widows (not image) --> 
 <div class="logInPanel">
   <div class="logInImg">
@@ -20,13 +21,13 @@
     <img src="img/islandBackground.jpg" alt="Desert island">
   </div>
   <!-- discuss log-in processing for php --->
-  <form action="/login_process.php" method="post">
+  <form id="logIn" action="logIn.php" method="post">
     <div class="container" id="logInBoxes" >
       <label for="userName"><b>User Name:</b></label>
-      <input type="text" placeholder="Enter Username" name="userName" required></br>
+      <input type="text" placeholder="Enter Username" name="userName" id="userName" required></br>
 
       <label for="pswd"><b>Password:</b></label>
-      <input type="password" placeholder="Enter Password" name="pswd" required></br>
+      <input type="password" placeholder="Enter Password" name="pswd" id="pswd" required></br>
       <span class="pswd"><a href="#">Forget your password</a></span>
     </div>
 
@@ -41,5 +42,3 @@
 
 <?php require 'includes/footer.php'; ?>
 
-</body>
-</html>
