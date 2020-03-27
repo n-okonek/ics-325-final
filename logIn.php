9b077b7ca245940ee64e4514b7551ecd5a033c38
Nick Okonek <nickokonek@gmail.com>
2020-03-27 12:40:29 -0500

    updated styles for login page

--- logIn.php
+++ logIn.php
@@ -3,15 +3,11 @@
   require 'includes/body.php';
   require 'includes/header.php';
 
-  $PageTitle = "Your Expeditions Start Here"
+  $PageTitle = "Login To Your Account"
 ?>
     
   <div class="page-title">
     <h2><?php echo $PageTitle ?></h2>
-  </div>
-
-  <div class="logInBanner">
-    <h2>LOGIN TO YOUR ACCOUNT</h2>
   </div>
 
 <!-- div container for all content in log-in widows (not image) --> 
@@ -21,19 +17,19 @@
     <img src="img/islandBackground.jpg" alt="Desert island">
   </div>
   <!-- discuss log-in processing for php --->
-  <form id="logIn" action="logIn.php" method="post">
+  <form id="login" action="login.php" method="post">
     <div class="container" id="logInBoxes" >
       <label for="userName"><b>User Name:</b></label>
       <input type="text" placeholder="Enter Username" name="userName" id="userName" required></br>
 
       <label for="pswd"><b>Password:</b></label>
       <input type="password" placeholder="Enter Password" name="pswd" id="pswd" required></br>
-      <span class="pswd"><a href="#">Forget your password</a></span>
+      <span class="pswd"><a href="#">Forget your password?</a></span>
     </div>
 
     <div class="container" id="submit">
       <button type="submit">Log-in</button>
-      <span class="createAccount"><a href="#">Create an account</a></span>
+      <span class="createAccount"><a href="./userRegister.php">Create an account</a></span>
     </div>
   </form>
 </div>
