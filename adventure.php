438ce88a32cd00a6837eb32f5fa68f801007f845
Nick Okonek <nickokonek@gmail.com>
2020-04-06 14:54:18 -0500

    added login page

--- adventure.php
+++ adventure.php
@@ -1,17 +1,47 @@
 <?php
 require("includes/page.php");
 
-$adventure = new Page();
 $PageTitle = "Kangerlussuaq, Greenland (North America)"; //replace with SQL Query pull from page table
 $Location = "Kangerlussuaq, Greenland"; //replace with SQL Query pull from expeditions table
 $Description = "{ContentSummary}"; //replace with SQL Query pull from expeditions table
 $expImg = "Kangerlussuaq.jpg"; //replace with SQL Query pull from page table
 $expImgAlt = "Glaciers of Kangerlussuaq"; //replace with SQL Query
 $map = file_get_contents('img/map.php');
-$reviews = [["headline1", "Content", "user"],
-            ["headline2", "Content", "user"],
-            ["headline3", "Content", "user"]
-          ]; //this array will be replaced with a sql query that creates a table of reviews for the location being viewed
+
+
+class AdventurePage extends Page{
+  public $reviews = [["headline1", "Content", "user"],
+              ["headline2", "Content", "user"],
+              ["headline3", "Content", "user"]
+            ]; //this array will be replaced with a sql query that creates a table of reviews for the location being viewed
+
+  public function Display(){
+    $this -> DisplayHead(); // includes all meta information including site title and page names
+    $this -> DisplayBody();
+    $this -> DisplayHeader(); //includes display menu
+    echo $this->content;
+    $this->ReviewGen($this->reviews);
+    $this -> DisplayFooter();
+  }
+
+  public function ReviewGen($reviews){
+    ?><section class='reviews'>
+    <h3>User Reviews</h3>
+    
+    <?php
+    for ($a=0; $a < count($reviews); $a++ ) {
+      echo "<div class='top-review'>
+        <div class='review-content'>
+          <h4>".$reviews[$a][0]."</h4>
+          <p>".$reviews[$a][1]."</p>
+        </div>
+      </div>";
+      }
+      ?></section><?php
+    }
+}
+
+$adventure = new AdventurePage();
 
 $adventure->content ="
 <section class='page-title'>
@@ -23,22 +53,8 @@ $adventure->content ="
 </div>
 
 <section class='main-content'>".$Description."
-</section>
-
-<section class='reviews'>
-<h3>User Reviews</h3>".ReviewGen($reviews)."
 </section>";
 
 $adventure->Display();
 
-function ReviewGen($reviews){
-  for ($a=0; $a < count($reviews); $a++ ) {
-    echo "<div class='top-review'>
-      <div class='review-content'>
-        <h4>".$reviews[$a][0]."</h4>
-        <p>".$reviews[$a][1]."</p>
-      </div>
-    </div>";
-    }
-  }
 ?>
---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+require("includes/page.php");
+
+$homepage = new Page();
+$PageTitle = "Login To Your Account"; //replace with SQL Query
+$ReviewHeadline = "{Review Headline}"; //replace with SQL Query
+$ReviewContent = "{Review Content}"; //replace with SQL Query
+$BgImg = "islandBackground.jpg"; //replace with SQL Query
+$BgImgAlt = "Desert island"; //replace with SQL Query
+
+$homepage->content ="
+<section class='page-title'>
+<h2>".$PageTitle."</h2>
+</section>
+
+<div class='content-panel'>
+  <div class='panel-img'>
+    <!-- just pic for reference  -->
+    <img src='img/".$BgImg."' alt='".$BgImgAlt."'>
+  </div>
+  <!-- discuss log-in processing for php --->
+  <form id='login' action='./includes/auth.php' method='post' onSubmit='return validate();'>
+    <div class='container' id='logInBoxes' >
+      <label for='userName'><b>User Name:</b></label><span id='user_info' class='error-info'></span>
+      <input class='form-control' type='text' placeholder='Enter Username' name='userName' id='userName' required></br>
+
+      <label for='pswd'><b>Password:</b></label><span id='password_info' class='error-info'></span>
+      <input class='form-control' type='password' placeholder='Enter Password' name='pswd' id='pswd' required></br>
+      
+    </div>
+
+    <div class='container' id='submit'>
+      <button type='submit'>Log-in</button>
+      <span class='pswd'><a href='#'>Forget your password?</a></span> | <span class='createAccount'><a href='./register.php'>Create an account</a></span>
+    </div>
+  </form>
+
+</div>
+";
+
+$homepage->Display();
+?>
