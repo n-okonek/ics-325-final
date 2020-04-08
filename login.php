ca0992b93f858ee33626b7f10dadf04b80357536
Nick Okonek <nickokonek@gmail.com>
2020-04-08 17:59:23 -0500

    added some backend stuff

--- login.php
+++ login.php
@@ -1,10 +1,53 @@
 <?php
 require("includes/page.php");
 
-$login = new Page();
+
 $PageTitle = "Login To Your Account"; //replace with SQL Query
 $BgImg = "islandBackground.jpg"; //replace with SQL Query
 $BgImgAlt = "Desert island"; //replace with SQL Query
+
+class LoginPage extends Page{
+  public function Display(){
+    session_start();
+    $this -> DisplayHead(); // includes all meta information including site title and page names
+    $this -> DisplayBody();
+    $this -> DisplayHeader($this->buttons); //includes display menu
+    echo $this->content;
+    $this -> DisplayLogin();
+    $this -> DisplayFooter();
+  }
+
+  public function DisplayLogin(){
+    ?>  <!-- discuss log-in processing for php --->
+    <form id='login' action='./includes/auth.php' method='post' onSubmit='return validate();'>
+      <?php
+        if (isset($_SESSION["errorMessage"])){
+          ?>
+          <div class="alert alert-warning" role="alert"><?= $_SESSION["errorMessage"];?></div>
+          <?php unset($_SESSION["errorMessage"]);
+        }
+      ?>
+      <div class='container' id='logInBoxes' >
+        <label for='userName'><b>Email Address:</b></label><span id='user_info' class='error-info'></span>
+        <input class='form-control' type='text' placeholder='Enter Email Address' name='userName' id='userName' required></br>
+  
+        <label for='pswd'><b>Password:</b></label><span id='password_info' class='error-info'></span>
+        <input class='form-control' type='password' placeholder='Enter Password' name='pswd' id='pswd' required></br>
+        
+      </div>
+  
+      <div class='container' id='submit'>
+        <button type='submit' name="login" value="Login">Log-in</button>
+        <span class='pswd'><a href='#'>Forget your password?</a></span> | <span class='createAccount'><a href='./register.php'>Create an account</a></span>
+      </div>
+    </form>
+  
+  </div>
+  <?php
+  }
+}
+
+$login = new LoginPage();
 
 $login->content ="
 <section class='page-title'>
@@ -16,24 +59,6 @@ $login->content ="
     <!-- just pic for reference  -->
     <img src='img/".$BgImg."' alt='".$BgImgAlt."'>
   </div>
-  <!-- discuss log-in processing for php --->
-  <form id='login' action='./includes/auth.php' method='post' onSubmit='return validate();'>
-    <div class='container' id='logInBoxes' >
-      <label for='userName'><b>User Name:</b></label><span id='user_info' class='error-info'></span>
-      <input class='form-control' type='text' placeholder='Enter Username' name='userName' id='userName' required></br>
-
-      <label for='pswd'><b>Password:</b></label><span id='password_info' class='error-info'></span>
-      <input class='form-control' type='password' placeholder='Enter Password' name='pswd' id='pswd' required></br>
-      
-    </div>
-
-    <div class='container' id='submit'>
-      <button type='submit'>Log-in</button>
-      <span class='pswd'><a href='#'>Forget your password?</a></span> | <span class='createAccount'><a href='./register.php'>Create an account</a></span>
-    </div>
-  </form>
-
-</div>
 ";
 
 $login->Display();
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,7 @@
 function validate() {
   var $valid = true;
-  document.getElementById("user_info").innerHTML = "";
-  document.getElementById("password_info").innerHTML = "";
+  document.getElementById("userName").innerHTML = "";
+  document.getElementById("pswd").innerHTML = "";
   
   var userName = document.getElementById("userName").value;
   var password = document.getElementById("pswd").value;
