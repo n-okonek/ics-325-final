840cf8f2797a071b07bbbcbdbeb5039ba1dfd948
Nick Okonek <nickokonek@gmail.com>
2020-04-16 10:10:04 -0500

    started pulling in reviews to city pages

--- adventure.php
+++ adventure.php
@@ -3,37 +3,56 @@ require("includes/page.php");
 
 class AdventurePage extends Page{
 
-
-  public $reviews = [["headline1", "Content", "user"],
-              ["headline2", "Content", "user"],
-              ["headline3", "Content", "user"]
-            ]; //this array will be replaced with a sql query that creates a table of reviews for the location being viewed
-
   public function Display($pageID){
     $this -> DisplayHead(); // includes all meta information including site title and page names
     $this -> DisplayBody();
     $this -> DisplayHeader($this->buttons); //includes display menu
     $this->  DisplayContent();
-    $this->ReviewGen($this->reviews);
+    $this->ReviewGen();
     $this -> DisplayFooter();
   }
 
-  public function ReviewGen($reviews){
-    ?><section class='reviews'>
+  public function ReviewGen(){
+    $db = new mysqli('localhost', 'glazpmck_ics325_web', 'ICS325.01-2020', "glazpmck_ics325");
+      
+    if ($db -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $db -> connect_error;
+      exit();
+    }
+    
+    $id = $db->real_escape_string($_GET['pid']);
+    $query = "SELECT reviewlist.ReviewHeadline, reviewlist.Review, reviewlist.User_ID, users.fname, reviewlist.city, reviewlist.rating 
+    FROM reviewlist
+    INNER JOIN users ON reviewlist.user_id = users.user_ID
+    INNER JOIN sitemap ON reviewlist.City = sitemap.pageID
+    WHERE reviewlist.city = '$id'";
+    $result = $db->query($query);
+    $row=$result->fetch_array(MYSQLI_ASSOC);
+    ?>
+    
+    <section class='reviews'>
     <h3>User Reviews</h3>
     
     <?php
-    for ($a=0; $a < count($reviews); $a++ ) {
-      echo "<div class='top-review'>
+    
+    echo "<section class='top-reviews'>";
+
+    while ($row){
+      ?>
+      <div class='top-review'>
         <div class='review-content'>
-          <h4>".$reviews[$a][0]."</h4>
-          <p>".$reviews[$a][1]."</p>
+          <h3><?=$headline?> - <?=$rating?>/5</h3> 
+          <p><?= $city.", ".$country?><br /><?=$review?></p><p>- <?=$name?></p>
         </div>
-      </div>";
-      }
-      ?></section>
-    <?php
-  }
+      </div>
+      <?php
+    }
+
+    echo "</section>";
+
+  $stmt->free_result();
+  $db->close();
+}
 
   public function DisplayContent(){
     if (isset($_GET['pid'])){
