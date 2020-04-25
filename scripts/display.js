b5684ff5476908df29f469ce0aae76de533164fb
Nick Okonek <nickokonek@gmail.com>
2020-04-25 10:46:59 -0500

    reset forms after cancel, ud jq selector on review

--- scripts/display.js
+++ scripts/display.js
@@ -11,38 +11,43 @@ function addJourney(){
   $('.add-journey').fadeIn();
 }
 
+function closeForm(form){
+  $("#".concat(form))[0].reset();
+  $(".".concat(form)).fadeOut();
+}
+
 function showExpeditions(value){
   $('#expedition, #journeyExpedition').find('option:first').attr('selected', 'selected');
   $('.expedition-selector').fadeIn();
   switch (value){
     case 'AU':
       $('#expedition > option, #journeyExpedition > option').hide();
-      $('option[value="ALICES"], option[value="YULARA"]').show();
+      $('option[value="10"]').show();
       break;
     
     case 'DE':
       $('#expedition > option, #journeyExpedition > option').hide();
-      $('option[value="BADKIS"], option[value="BERLIN"]').show();
+      $('option[value="8"]').show();
       break;
 
     case 'GL':
-      $('#expedition > option, #journeyExpedition > option').attr('hidden');
-      $('option[value="KANGER"], option[value="QEQERT"]').removeAttr('hidden');
+      $('#expedition > option, #journeyExpedition > option').hide();
+      $('option[value="7"]').show();
       break;
 
-    case 'CF':
-      $('#expedition > option, #journeyExpedition > option').addAttr('hidden');
-      $('option[value="KAZUMB"], option[value="MWANZA"]').removeAttr('hidden');
+    case 'TZ':
+      $('#expedition > option, #journeyExpedition > option').hide();
+      $('option[value="13"]').show();
       break;
 
     case 'US':
-      $('#expedition > option, #journeyExpedition > option').attr('hidden');
-      $('option[value="LOSANG"], option[value="SANDIE"]').removeAttr('hidden');
+      $('#expedition > option, #journeyExpedition > option').hide();
+      $('option[value="12"]').show();
       break;
 
     case 'RU':
-      $('#expedition > option, #journeyExpedition > option').attr('hidden');
-      $('option[value="SAMBUR"], option[value="SNEZHN"]').removeAttr('hidden');
+      $('#expedition > option, #journeyExpedition > option').hide();
+      $('option[value="16"]').show();
       break;
     default: 
       $('.expedition-selector').hide();
