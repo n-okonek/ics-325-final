679addb5f98e8138ea56aa398bf30de9485dc938
Nick Okonek <nickokonek@gmail.com>
2020-04-26 22:25:58 -0500

    added error messaging on user ud

--- includes/page.php
+++ includes/page.php
@@ -223,6 +223,11 @@ class Page{
                     <div class="alert alert-success" role="alert"><?= $_SESSION["successMessage"];?></div>
                     <?php unset($_SESSION["successMessage"]);
                   }
+                  if (isset($_SESSION["errorMessage"])){
+                    ?>
+                    <div class="alert alert-warning" role="alert"><?= $_SESSION["errorMessage"];?></div>
+                    <?php unset($_SESSION["errorMessage"]);
+                  }
                 ?>
               </section>
             <?php
