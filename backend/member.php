feaa30f9dd8e52fe5263adf2baf908b3fddfc203
Nick Okonek <nickokonek@gmail.com>
2020-05-03 08:25:14 -0500

    added backend code to reset password

--- backend/member.php
+++ backend/member.php
@@ -55,5 +55,138 @@ class Member
         }else {return 0;}
         $this->db->close();
     }
+
+    public function PwResetReq($email){
+        $selector = bin2hex(random_bytes(8));
+	    $token = random_bytes(32);
+
+        $expires - date("U") + 1800;
+
+        $sql = "DELETE FROM pwdreset WHERE pwdResetEmail=?;";
+	    $stmt = $this->db->prepare($sql);
+            if (!$this->db) {
+                $_SESSION['errorMessage'] = "Our hamster fell off the wheel, please try again.";
+                exit();
+            } else {
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+            }
+        
+        $sql = "INSERT INTO pwdreset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) VALUES (?, ?, ?, ?);";
+        $stmt = $this->db->prepare($sql);
+        if (!$this->db) {
+            $_SESSION['errorMessage'] = "Our hamster fell off the wheel, please try again.";
+            exit();
+        } else {
+            $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+            $stmt->bind_param("ssss", $email, $selector, $hashedToken, $expires);
+            $stmt->execute();
+        }
+        $this->db->close();
+        $email_reset = $this->SendResetMail($email, $selector, $token);
+        if ($email_reset){
+            return true;
+        }
+    }
+
+    public function SendResetMail($email, $selector, $token){
+        $to = $email;
+        $url = $_SERVER['SERVER_NAME']."/reset.php?selector=" . $selector . "&validator=" . bin2hex($token);
+		$subject = 'Wanderlust Outpost Password Reset Request';
+		
+		$message  = '<p> We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.</p>';
+		$message .= '<p> Here is your password reset link: </br>';
+		$message .= '<a href="' . $url . '">' . $url . '</a></p>';
+		
+		$headers = "From: Wanderlust Outpost <no-reply@ics325.glazedproductions.com>\r\n";
+		$headers .= "Reply-To: wonderlust email address\r\n";
+		$headers .= "Content-type: text/html\r\n";
+		
+        mail($to, $subject, $message, $headers);
+        return true;
+    }
+
+    public function ResetPassword($selector, $validator, $password, $passwordRepeat){
+
+	    $currentDate = date("U");
+		
+        $sql = "SELECT * FROM pwdreset WHERE pwdResetSelector = ? AND pwdResetExpires >= ?";
+        $stmt = $this->db->prepare($sql);
+        if (!$stmt) {
+            echo "There was an error!";
+            exit();
+        } 
+    	else {
+	    	$stmt->bind_params("ss", $selector, $currentDate);
+		    $stmt->execute($stmt, $currentDate);
+		
+		    $row = $stmt->fetch_array(MYSQLI_ASSOC);
+		    if (!$stmt->fetch_array(MYSQLI_ASSOC)) {
+			    echo "You need to re-submit your reset request.";
+			    exit();
+		    }
+            else {
+                $tokenBin = hex2bin($validator);
+                $tokenCheck = password_varify($tokenBin, $row["pwdResetToken"]);
+                
+                if($tokenCheck === false) {
+                    echo "You need to re-submit your reset request.";
+                    exit();
+                }
+                elseif ($tokenCheck === true) {
+                    
+                    $tokenEmail = $row['pwdResetEmail'];
+                    
+                    $sql = "SELECT * FROM users WHERE Email = ?;";
+                    $stmt =  $this->db->prepare($sql);
+                    
+                    if (!$stmt) {
+                        echo "There was an error!";
+                        exit();
+                    } 
+                    
+                    else {
+                        $stmt->bind_params("s", $tokenEmail);
+                        $stmt->execute();
+                        $results = $stmt->fetch_array(MYSQLI_ASSOC);
+                        
+                        if (!$row = $stmt->fetch_array(MYSQLI_ASSOC)) {
+                            echo "There was an error!";
+                            exit();
+                        }
+                        
+                        else {
+                            $sql = "UPDATE users SET Psword=? Where Email=?";
+                            $stmt = $this->db->prepare($sql);
+                            if (!$stmt) {
+                                echo "There was an error!";
+                                exit();
+                            }
+                            
+                            else {
+                                $newPwdHash = md5($password);
+                                $stmt->bind_params("ss", $newPwdHash, $tokenEmail);
+                                $stmt->execute();
+                                
+                                $sql = "DELETE FROM pwdreset WHERE pwdResetEmail=?;";
+                                $stmt = $this->db->prepare($sql);
+                                
+                                if (!$stmt) {
+                                    echo "There was an error!";
+                                    exit();
+                                }
+                                
+                                else {
+                                    $stmt->bind_params("s", $tokenEmail);
+                                    $stmt->execute();
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+		    }
+	    } 	
+    } 
 }
 ?>
